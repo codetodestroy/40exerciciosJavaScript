5519d11ea485f7410796f7862e817e0eba86ce06
Caio Douglas <caiodqribeiro@gmail.com>
2024-08-06 01:52:54 -0300

    Exercicio 5 feito

---
+++ js/exercicio05.js
@@ -0,0 +1,5 @@
+document.querySelector('button').addEventListener('click', function() {
+    var quantidadeCaracteres = document.querySelector('#nome').value.length;
+
+    alert(quantidadeCaracteres);
+});
