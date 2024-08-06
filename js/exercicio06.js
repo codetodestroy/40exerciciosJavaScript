419954cc410b3aa1bf52fc81c2ee7b2f7eab7098
Caio Douglas <caiodqribeiro@gmail.com>
2024-08-06 01:57:36 -0300

    Exercicio 6 feito

---
+++ js/exercicio06.js
@@ -0,0 +1,6 @@
+let lamp = document.querySelector('img');
+let lampOn = 'https://github.com/gabrieldarezzo/helpjs-ravi/blob/master/images/lampada-on.jpg?raw=true';
+
+lamp.addEventListener('click', function() {
+    lamp.src = lampOn;
+});
